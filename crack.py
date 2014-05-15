192b1bad14c3e1dc00eaa7a6291d2e0654307889
Fitblip <fitblip@gmail.com>
2014-05-15 02:36:10 -0700

    Update to work on windows with oclhashcat

--- crack.py
+++ crack.py
@@ -3,42 +3,48 @@ import sys
 import re
 
 startLength = 1
-maxPasswordLength = 8
-matchRE = re.compile('^\w{32}\:.*$')
+maxPasswordLength = 10
+matchRE = re.compile('.*(\w{32}\:\S+@.*)$',re.MULTILINE)
+
+hashcatBin = "C:\\temp\\hashcat\\oclHashcat-1.20\\oclHashcat64.exe"
 
 emailProviders = [
 	"gmail.com",
-	"aol.com",
-	"yahoo.com",
-	"hotmail.com",
+#	"aol.com",
+#	"yahoo.com",
+#	"hotmail.com",
 ]
 
 for i in range(startLength,maxPasswordLength+1):
 	for email in emailProviders:
 		email = "@" + email
-		maskPattern = "?l" * i
+		maskPattern = "?1" * i
 
 		mask = maskPattern + email
-		print "Cracking length %s using mask %s" % (i,mask)
+		print "\n\nCracking length %s using mask %s" % (i,mask)
 
 		commandLine = [
-					'/tmp/hashcat-0.47/hashcat-cli64.app',
+					hashcatBin,
 					'-m',
 					'0',
 					'-a',
 					'3',
-					'--quiet',
-					'--pw-min=%d' % (i + len(email)),
-					'/tmp/hash.txt',
+					'-1',
+					'?l?u?d_.',
+		  			'hash.txt',
 					str(mask)
 				]
-
 		proc = subprocess.Popen(commandLine, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		#print "Ran %s" % " ".join(commandLine)
+		print "Ran %s\n\n" % " ".join(commandLine)
 		
 		for line in iter(proc.stdout.readline, ""):
-		    if matchRE.match(line):
-		    	print "Hash found! %s" % line
-		    	sys.exit(0)
-		    elif line.startswith('Progress') or line.startswith('Estimated'):
-		    	print line,
+			if matchRE.match(line):
+				print "\n\n==============="
+				print "Hash found! %s" % matchRE.match(line).groups()[0]
+				print "==============="
+				sys.exit(0)
+			else:
+				print line,
+
+		import time
+		#time.sleep(1)
